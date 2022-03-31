89e84e0a9a447e907c6370dd8dec597714401f8f
Jean-Maxime Dereuder <101884584+Maxime45470@users.noreply.github.com>
2022-03-31 16:16:21 +0200

    tp4 - erreur a trouver

---
+++ Jean-Maxime/tp4.js
@@ -0,0 +1,11 @@
+let x = 5;
+let y = 10;
+
+let message = document.getElementById("p1");
+
+if(x > y) {
+    message.innerHTML(`${x} est plus grand que ${y}`);
+}
+else if(x<=y) {
+    message.innerHTML(`${x} est plus petit que ${y}`);
+}
