cd1763f70a160ceeca29f3b11014914f42c40f80
Thomas <92104928+thomasvcnh45@users.noreply.github.com>
2022-03-31 14:36:08 +0200

    Add files via upload

--- Thomas/tp3 thomas.js
+++ Thomas/tp3 thomas.js
@@ -1,5 +1,6 @@
 let nombre = 50;
 let nombre2 = 3; 
 let resultat = 50/3;
+let reste = 50%3;
 
-alert(`Le résultat de ${nombre}/${nombre2} est de ${resultat}`)
+alert(`Le résultat de ${nombre}/${nombre2} est de ${resultat} et le reste est de ${reste}`);
