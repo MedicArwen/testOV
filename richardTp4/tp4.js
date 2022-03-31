955149c8b9edaeec41be009e4e9535db0583015f
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2022-03-31 17:09:18 +0200

    tp4

---
+++ richardTp4/tp4.js
@@ -0,0 +1,30 @@
+
+
+
+    let x = 5;
+    let y = 10;
+
+    if (x < y) {
+         document.getElementById('p1').innerHTML = "x est plus petit que y";
+        
+
+    } 
+    if (x > y) {
+        document.getElementById('p1').innerHTML = "x est plus grand que y";
+       
+       
+
+   }
+   
+    
+
+
+
+
+
+
+
+
+
+
+    
